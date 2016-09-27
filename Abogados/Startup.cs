af97fb9904257813f84cfd9892e57a7e891f4450
usuario <usuario@WIN-KDJVGTK41KK>
2016-09-26 22:29:42 -0300

    Add project files.

---
+++ Abogados/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Abogados.Startup))]
+namespace Abogados
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
