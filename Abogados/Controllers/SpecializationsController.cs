06833603a74879564d1afc38775df819f7f1a8ba
Gabriel Tassone <gabrieltassone3@hotmail.com>
2016-09-29 14:50:51 -0300

    Commit 29/09

---
+++ Abogados/Controllers/SpecializationsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Abogados.Models;
+
+namespace Abogados.Controllers
+{
+    public class SpecializationsController : Controller
+    {
+        // GET: Specializations
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Especializaciones()
+        {
+            return View(new SpecializationModel { SpecializationId = 1 });
+        }
+
+        [HttpPost]
+        public ActionResult Especializaciones(SpecializationModel especializacion)
+        {
+            var especializacion1 = especializacion;
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Abogados/Models/OrganizationModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Abogados.Models
+{
+    public class OrganizationModel
+    {
+        //ver si dejar o no esta entidad. 
+
+        public int OrganizationId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Mission { get; set; }
+
+        public string Vision { get; set; }
+
+        public string History { get; set; }
+
+
+    }
+}
