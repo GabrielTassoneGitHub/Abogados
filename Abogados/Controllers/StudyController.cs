329d126ee4961428fc155e593c8a34930f244233
Gabriel Tassone <gabrieltassone3@hotmail.com>
2016-10-05 09:18:23 -0300

    Commit 05/10

---
+++ Abogados/Controllers/StudyController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Abogados.Controllers
+{
+    public class StudyController : Controller
+    {
+        // GET: Study
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Study/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Study/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Study/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Study/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Study/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Study/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Study/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
