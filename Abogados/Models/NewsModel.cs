c0be0fdbcf147b6b0691cdd08ec2788bcce67124
Gabriel Tassone <gabrieltassone3@hotmail.com>
2016-10-13 15:09:01 -0300

    Commit 13/10

---
+++ Abogados/Models/NewsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Abogados.Models
+{
+    public class NewsModel
+    {
+        public int NewsId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+    }
+}
